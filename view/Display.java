88e1c731868c4e47e27c09c444a36b6c2bcca44f
gokcepaz <33886772+gokcepaz@users.noreply.github.com>
2018-03-16 13:48:05 +0300

    Create Display.java

---
+++ view/Display.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+import controller.TaskController;
+
+/**
+ *
+ * @author Gokce 
+ */
+public class Display {
+    
+    
+}
+© 2018 GitHub, Inc.
